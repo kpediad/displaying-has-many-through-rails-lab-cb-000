307df53164a72ba045fa44b509af4f8faab2bd04
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-05 10:39:58 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def human_datetime
+    self.appointment_datetime.strftime("%B %-d, %Y at %H:%M")
+  end
 end
